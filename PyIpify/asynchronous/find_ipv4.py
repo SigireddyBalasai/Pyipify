6479c85e3294176f21ab98d521afc5a87507c58b
SigireddyBalasai <101649913+SigireddyBalasai@users.noreply.github.com>
2023-06-27 19:28:09 +0530

    Revert "merge collapsible `with` statements"

--- PyIpify/asynchronous/find_ipv4.py
+++ PyIpify/asynchronous/find_ipv4.py
@@ -13,5 +13,6 @@ async def find_ipv4():
         >>> from PyIpify.asynchronous import find_ipv4
         >>> asyncio.run(find_ipv4())
     """
-    async with aiohttp.ClientSession() as session, session.get('https://api.ipify.org?format=json') as response:
-        return (await response.json())['ip']
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.ipify.org?format=json') as response:
+            return (await response.json())['ip']
